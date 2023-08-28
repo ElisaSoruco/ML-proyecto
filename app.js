fa5e1c267fd6af3787bd260b8f0e67eb682b3752
ElisaSoruco <elisasoruco28@gmail.com>
2023-08-27 21:57:02 -0300

    css del login y register mejorados

--- app.js
+++ app.js
@@ -14,9 +14,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
 })
 
-app.get('/product', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/product.html'))
-})
 
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
